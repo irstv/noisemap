6155eb867d3384859f0fda9819301fcb229624fd
nicolas-f <1382241+nicolas-f@users.noreply.github.com>
2024-11-20 15:10:17 +0100

    Update conf.py

--- Docs/conf.py
+++ Docs/conf.py
@@ -46,8 +46,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'sphinx.ext.autosectionlabel',
-    'sphinx_rtd_theme',
+    'sphinx.ext.autosectionlabel'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
